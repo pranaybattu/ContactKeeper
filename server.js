f05bc047da2984c06f37995f4354a0e74034014b
pranaybattu <battupranay1999@gmail.com>
2021-06-03 23:12:22 +0530

    Added- Backend- (Users,contacts)models & jwt Authentication

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
+
+app.use(express.json({extended:false}))
+
+connectDB();
 
 app.get('/', (req,res) => res.json({msg:'Welcome to ContacKeeper API...'}) )
 
